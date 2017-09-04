281979abe1815bcbe65ca9fce58dc8c18d6397ea
Sergey <xapepam@gmail.com>
2017-09-04 17:19:39 +0300

    add one example string

--- 6/adder_func.py
+++ 6/adder_func.py
@@ -1,5 +1,6 @@
 def adder(a,b):
     print (a+b)
+adder (2,3)
 adder ('str1','str2')
 adder ([1,2,3],[4,5,6])
 adder (2.0,5.4)
--- 6/adder_funс_many_args.py
+++ 6/adder_funс_many_args.py
@@ -1,9 +1,9 @@
 def adder(*a):
     for x in a:
-        union(*a)
-adder ('str1','str2','str3')
+       print(sum(x))
+# adder ('str1','str2','str3')
 adder ([1,2,3],[4,5,6])
-adder (2.0,5.4)
+# adder (2.0,5.4)
 input("Press enter to exit ;)")
         
     
