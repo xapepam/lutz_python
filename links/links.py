8d7a6d1f169d727afc4175d049528afb5e23ca94
Sergey <xapepam@gmail.com>
2017-09-05 18:18:32 +0300

    error

--- links/links.py
+++ links/links.py
@@ -1,6 +1,6 @@
 import webbrowser
 while True:
-    print ('Hello this is sorage and link-selecter \n')
+    print ('Hello this is storage and link-selecter \n')
     print ('\n')
     print ('Choose number to open link: \n 1. Bublesort \n 2. Selectsort\n 3. epmty \n 4. empty \n \n 0. Exit \n')
     x = int(input('Enter a number: '))
