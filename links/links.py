54bbced0ebaec0478bc34aa8f627c741a3837287
Sergey <xapepam@gmail.com>
2017-09-05 18:15:08 +0300

    add

--- links/links.py
+++ links/links.py
@@ -2,8 +2,10 @@ import webbrowser
 while True:
     print ('Hello this is sorage and link-selecter \n')
     print ('\n')
-    print ('Choose number to open link: \n 0. Exit \n 1.Bublesort \n 2.Selectsort\n 3. epmty \n 4. empty \n')
-    x = input('Enter a number: ')
-    if x < 1:
-      webbrowser.open('http://py-algorithm.blogspot.ru/2011/10/blog-post.html') 
+    print ('Choose number to open link: \n 1. Bublesort \n 2. Selectsort\n 3. epmty \n 4. empty \n \n 0. Exit \n')
+    x = int(input('Enter a number: '))
+    if x == 1:
+      webbrowser.open('http://py-algorithm.blogspot.ru/2011/10/blog-post.html')
+    if x == 2:
+      webbrowser.open('http://ya.ru')
 
